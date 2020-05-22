ddc0244b79e9c13e33730231a97cb5a17b2f28b7
github username <github email address>
2020-05-22 16:07:49 -0400

    checking if events work

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,11 @@
 class EventsController < ApplicationController
 
-
+    def index
+        @events = Event.all
+        render json: @events.to_json(
+            :include => [:ets]
+        )
+    end
 
     def create
         
